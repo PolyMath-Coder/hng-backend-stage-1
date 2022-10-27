67777d115a47359aaf376450eaf1b38724d23f84
Ayooluwa Adeleke <51849584+PolyMath-Coder@users.noreply.github.com>
2022-10-27 11:09:52 +0100

    add needful package

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-const PORT = 3000;
+require('dotenv').config();
+const PORT = process.env.PORT;
 
 const app = express();
 
 app.use(cors({ origin: '*' }));
 
 app.get('/', (req, res) => {
-  res
-    .status(201)
-    .json({
-      slackUsername: 'PolyMath-Coder',
-      backend: true,
-      age: 20,
-      bio:
-        'A bug is a feature waiting to be discovered... All I do is fix them!',
-    });
+  res.status(201).json({
+    slackUsername: 'PolyMath-Coder',
+    backend: true,
+    age: 20,
+    bio: 'A bug is a feature waiting to be discovered... All I do is fix them!',
+  });
 });
 
 app.listen(PORT, () => {
