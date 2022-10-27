8af8bb1079b3a3cb0a6fce15419a8cdd8adfa1ca
Ayooluwa Adeleke <51849584+PolyMath-Coder@users.noreply.github.com>
2022-10-27 11:02:35 +0100

    complete first task

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const PORT = 3000;
+
+const app = express();
+
+app.use(cors({ origin: '*' }));
+
+app.get('/', (req, res) => {
+  res
+    .status(201)
+    .json({
+      slackUsername: 'PolyMath-Coder',
+      backend: true,
+      age: 20,
+      bio:
+        'A bug is a feature waiting to be discovered... All I do is fix them!',
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server now live at port ${PORT}`);
+});
